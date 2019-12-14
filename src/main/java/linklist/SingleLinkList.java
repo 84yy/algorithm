59d946a676e81383b2c07a865446e9de8a2cbb7d
Simon <baxi84yy@163.com>
2019-12-14 09:47:35 +0800

    使用单链表实现了回文字符串的判断

--- src/main/java/linklist/SingleLinkList.java
+++ src/main/java/linklist/SingleLinkList.java
@@ -5,6 +5,7 @@ package linklist;
  *
  * @author hzm
  */
+@SuppressWarnings("all")
 public class SingleLinkList {
 
 
@@ -19,14 +20,55 @@ public class SingleLinkList {
      */
     public void addTail(SingleLinkListNode node) {
         //如果链表没有初始化,把要插入的节点设置位头节点
-        if (this.root == null) {
+        SingleLinkListNode crtNode = this.root;
+        if (crtNode == null) {
             this.root = node;
             return;
         }
-        if (this.root.hasNext()) {
-
+        //获取尾节点
+        while (crtNode.hasNext()) {
+            crtNode = crtNode.next;
         }
+        crtNode.next = node;
     }
+
+    /**
+     * 移除并返回头结点
+     *
+     * @return
+     */
+    public SingleLinkListNode getAndRemoveHead() {
+        if (this.root == null) {
+            return null;
+        }
+        SingleLinkListNode rmNode = this.root;
+        this.root = this.root.next;
+        return rmNode;
+    }
+
+    /**
+     * 移除并返回尾结点
+     */
+    public SingleLinkListNode getAndRemoveTail() {
+        if (this.root == null) {
+            return null;
+        }
+        SingleLinkListNode crtNode = this.root;
+        //通过判断节点的下联节点是否有下联节点,识别当前节点的下个节点是否为尾部
+        while (crtNode.hasNext() && crtNode.next.hasNext()) {
+            crtNode = crtNode.next;
+        }
+
+        SingleLinkListNode tailNode = null;
+        if (crtNode.hasNext()) {
+            tailNode = crtNode.next;
+        } else {
+            tailNode = crtNode;
+        }
+        crtNode.next = null;
+        return tailNode;
+    }
+
 
     /**
      * 在头部添加
@@ -48,12 +90,11 @@ public class SingleLinkList {
         /**
          * 节点存储的字符
          */
-        private char c;
+        private char data;
         /**
          * 节点的下联节点
          */
         private SingleLinkListNode next;
-
 
         public boolean hasNext() {
             return this.next != null;
@@ -64,7 +105,7 @@ public class SingleLinkList {
          */
         public SingleLinkListNode(char c) {
             super();
-            this.c = c;
+            this.data = c;
         }
 
         /**
@@ -82,5 +123,46 @@ public class SingleLinkList {
         public SingleLinkListNode getNext() {
             return this.next;
         }
+
+        public char getData() {
+            return this.data;
+        }
+    }
+
+    /**
+     * 判断字符串是否回文
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        String str = args[0];
+        if (str == null || str.length() == 0) {
+            System.out.println("请输入有效的字符串...");
+            return;
+        }
+
+        SingleLinkList sglList = new SingleLinkList();
+        char[] chars = str.toCharArray();
+        //字符串存入链表
+        int len = chars.length;
+        if (len < 2) {
+            System.out.println("字符串" + str + "长度小于 2,必定是回文字符串...");
+            return;
+        }
+        for (int i = 0; i < len; i++) {
+            SingleLinkListNode sglNode = new SingleLinkListNode(chars[i]);
+            sglList.addTail(sglNode);
+        }
+
+        int halfLen = len >> 1;
+        for (int i = 0; i < halfLen; i++) {
+            SingleLinkListNode head = sglList.getAndRemoveHead();
+            SingleLinkListNode tail = sglList.getAndRemoveTail();
+            if (head.getData() != tail.getData()) {
+                System.out.println("字符串" + str + "不是回文...");
+                return;
+            }
+        }
+        System.out.println("字符串" + str + "是回文...");
     }
 }
